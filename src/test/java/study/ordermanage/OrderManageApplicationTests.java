7da0fdc16a367dda050db32e745d18402b90c344
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-06-10 12:51:33 +0900

    project init

---
+++ src/test/java/study/ordermanage/OrderManageApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ordermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
